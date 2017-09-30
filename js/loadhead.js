e6fe1c872c189ab99210ab850e020be284137ab6
Re-Creation Developers ADMIN <recreationdevelopers@gmail.com>
2017-09-30 21:01:23 +0530

    Added files and folder via Upload

---
+++ js/loadhead.js
@@ -0,0 +1,30 @@
+// ALL HEAD ELEMETS ADD HERE. LINE BY LINE AS YOU WANT IN THE PAGE
+
+// favicon
+$('head').append('<link rel="shortcut icon" href="http://renovaworld.com/favicon.ico" type="image/x-icon">');
+
+// Reveal form stuff
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/signupjs/jquery.reveal.js"></script>');
+$('head').append('<link rel="stylesheet" type="text/css" href="http://renovaworld.com/signupjs/reveal.css">');
+
+// Go to top
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/arrow55.js"></script>');
+
+// All main JS
+$('head').append('<!--[if lte IE 8]><script type="text/javascript" src="http://renovaworld.com/css/ie/html5shiv.js"></script><![endif]-->');
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/jquery-1.11.1.min.js"></script>'); // Jquery 1.11
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/jquery.dropotron.min.js"></script>');
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/jquery.scrolly.min.js"></script>');
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/jquery.onvisible.min.js"></script>');
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/skel.min.js"></script>');
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/skel-layers.min.js"></script>');
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/init.js"></script>');
+
+// JS and CSS For Pagination
+$('head').append('<script type="text/javascript" src="http://renovaworld.com/js/jpaginate.js"></script>');
+$('head').append('<link rel="stylesheet" type="text/css" href="http://renovaworld.com/css/pagination.css">');
+
+// CSS Stuff
+$('head').append('<link rel="stylesheet" type="text/css" href="http://renovaworld.com/css/embeddresponsively.css">'); // responsive embedd
+$('head').append('<link rel="stylesheet" type="text/css" href="http://renovaworld.com/css/downloadbutton.css">'); // downloadbutton
+$('head').append('<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="http://renovaworld.com/css/ie/v8.css" /><![endif]-->');
