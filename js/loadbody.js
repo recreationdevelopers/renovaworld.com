e0589280631beb106ad7f6e751a4b1f0ff96cce3
Re-Creation Admin <recreationdevelopers@gmail.com>
2019-08-16 22:39:12 +0530

    https add-on and extra removal
    Added https for all files and removed data that is not needed on the site

--- js/loadbody.js
+++ js/loadbody.js
@@ -1,16 +1,16 @@
 $(function(){
-	$("#nav").load("http://renovaworld.com/common/navyo.html");  //Navigation bar (for later)
-	$("#contactusyo").load("http://renovaworld.com/common/contactusyo.html"); // Contact us Form
-	$("#newslettersignupyo").load("http://renovaworld.com/common/newslettersignupyo.html"); // Newsletter sign up form
-	$("#giftmeyo").load("http://renovaworld.com/common/giftmeyo.html"); // Gift me
-	$("#footer").load("http://renovaworld.com/common/footeryo.html"); // Footer
-	$("#rnmusicplayerdetails").load("http://renovaworld.com/common/rnmusicplayerdetails.html"); // Music Player Details
+	$("#nav").load("https://renovaworld.com/common/navyo.html");  //Navigation bar (for later)
+	$("#contactusyo").load("https://renovaworld.com/common/contactusyo.html"); // Contact us Form
+	$("#newslettersignupyo").load("https://renovaworld.com/common/newslettersignupyo.html"); // Newsletter sign up form
+	$("#giftmeyo").load("https://renovaworld.com/common/giftmeyo.html"); // Gift me
+	$("#footer").load("https://renovaworld.com/common/footeryo.html"); // Footer
+	$("#rnmusicplayerdetails").load("https://renovaworld.com/common/rnmusicplayerdetails.html"); // Music Player Details
 	$("#paginatekaro").jPaginate(); // Pagination load
-	$("#googleanalytics").load("http://renovaworld.com/common/analyticstracking.html"); // Google analytics
+	$("#googleanalytics").load("https://renovaworld.com/common/analyticstracking.html"); // Google analytics
 	
 	if ( $(window).width() > 670) {     
 		  //Add your javascript for large screens here
-		  $("#rnmusicplayer").load("http://renovaworld.com/common/rnmusicplayer.html"); // MUSIC PLAYER
+		  $("#rnmusicplayer").load("https://renovaworld.com/common/rnmusicplayer.html"); // MUSIC PLAYER
 		}
 		else {
 		  //Add your javascript for small screens here
