f51678c618bf8d822a10e932ebd38f328a0d5943
Re-Creation Admin <recreationdevelopers@gmail.com>
2019-08-17 23:28:25 +0530

    updated data

--- js/loadbody.js
+++ js/loadbody.js
@@ -1,16 +1,16 @@
 $(function(){
-	$("#nav").load("https://renovaworld.com/common/navyo.html");  //Navigation bar (for later)
-	$("#contactusyo").load("https://renovaworld.com/common/contactusyo.html"); // Contact us Form
-	$("#newslettersignupyo").load("https://renovaworld.com/common/newslettersignupyo.html"); // Newsletter sign up form
-	$("#giftmeyo").load("https://renovaworld.com/common/giftmeyo.html"); // Gift me
-	$("#footer").load("https://renovaworld.com/common/footeryo.html"); // Footer
-	$("#rnmusicplayerdetails").load("https://renovaworld.com/common/rnmusicplayerdetails.html"); // Music Player Details
+	$("#nav").load("common/navyo.html");  //Navigation bar (for later)
+	$("#contactusyo").load("common/contactusyo.html"); // Contact us Form
+	$("#newslettersignupyo").load("common/newslettersignupyo.html"); // Newsletter sign up form
+	$("#giftmeyo").load("common/giftmeyo.html"); // Gift me
+	$("#footer").load("common/footeryo.html"); // Footer
+	$("#rnmusicplayerdetails").load("common/rnmusicplayerdetails.html"); // Music Player Details
 	$("#paginatekaro").jPaginate(); // Pagination load
-	$("#googleanalytics").load("https://renovaworld.com/common/analyticstracking.html"); // Google analytics
+	$("#googleanalytics").load("common/analyticstracking.html"); // Google analytics
 	
 	if ( $(window).width() > 670) {     
 		  //Add your javascript for large screens here
-		  $("#rnmusicplayer").load("https://renovaworld.com/common/rnmusicplayer.html"); // MUSIC PLAYER
+		  $("#rnmusicplayer").load("common/rnmusicplayer.html"); // MUSIC PLAYER
 		}
 		else {
 		  //Add your javascript for small screens here
--- js/loadhead.js
+++ js/loadhead.js
@@ -1,30 +1,30 @@
 // ALL HEAD ELEMETS ADD HERE. LINE BY LINE AS YOU WANT IN THE PAGE
 
 // favicon
-$('head').append('<link rel="shortcut icon" href="https://renovaworld.com/favicon.ico" type="image/x-icon">');
+$('head').append('<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">');
 
 // Reveal form stuff
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/signupjs/jquery.reveal.js"></script>');
-$('head').append('<link rel="stylesheet" type="text/css" href="https://renovaworld.com/signupjs/reveal.css">');
+$('head').append('<script type="text/javascript" src="signupjs/jquery.reveal.js"></script>');
+$('head').append('<link rel="stylesheet" type="text/css" href="signupjs/reveal.css">');
 
 // Go to top
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/arrow55.js"></script>');
+$('head').append('<script type="text/javascript" src="js/arrow55.js"></script>');
 
 // All main JS
-$('head').append('<!--[if lte IE 8]><script type="text/javascript" src="https://renovaworld.com/css/ie/html5shiv.js"></script><![endif]-->');
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/jquery-1.11.1.min.js"></script>'); // Jquery 1.11
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/jquery.dropotron.min.js"></script>');
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/jquery.scrolly.min.js"></script>');
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/jquery.onvisible.min.js"></script>');
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/skel.min.js"></script>');
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/skel-layers.min.js"></script>');
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/init.js"></script>');
+$('head').append('<!--[if lte IE 8]><script type="text/javascript" src="css/ie/html5shiv.js"></script><![endif]-->');
+$('head').append('<script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>'); // Jquery 1.11
+$('head').append('<script type="text/javascript" src="js/jquery.dropotron.min.js"></script>');
+$('head').append('<script type="text/javascript" src="js/jquery.scrolly.min.js"></script>');
+$('head').append('<script type="text/javascript" src="js/jquery.onvisible.min.js"></script>');
+$('head').append('<script type="text/javascript" src="js/skel.min.js"></script>');
+$('head').append('<script type="text/javascript" src="js/skel-layers.min.js"></script>');
+$('head').append('<script type="text/javascript" src="js/init.js"></script>');
 
 // JS and CSS For Pagination
-$('head').append('<script type="text/javascript" src="https://renovaworld.com/js/jpaginate.js"></script>');
-$('head').append('<link rel="stylesheet" type="text/css" href="https://renovaworld.com/css/pagination.css">');
+$('head').append('<script type="text/javascript" src="js/jpaginate.js"></script>');
+$('head').append('<link rel="stylesheet" type="text/css" href="css/pagination.css">');
 
 // CSS Stuff
-$('head').append('<link rel="stylesheet" type="text/css" href="https://renovaworld.com/css/embeddresponsively.css">'); // responsive embedd
-$('head').append('<link rel="stylesheet" type="text/css" href="https://renovaworld.com/css/downloadbutton.css">'); // downloadbutton
-$('head').append('<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="https://renovaworld.com/css/ie/v8.css" /><![endif]-->');
+$('head').append('<link rel="stylesheet" type="text/css" href="css/embeddresponsively.css">'); // responsive embedd
+$('head').append('<link rel="stylesheet" type="text/css" href="css/downloadbutton.css">'); // downloadbutton
+$('head').append('<!--[if lte IE 8]><link rel="stylesheet" type="text/css" href="css/ie/v8.css" /><![endif]-->');
